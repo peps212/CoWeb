3ce942c227995841972f39c10c4a0d23de7eba26
peps212 <giuseppe.rapisarda12@gmail.com>
2023-08-23 13:52:57 +0100

    responsive design

--- pages/components/form.js
+++ pages/components/form.js
@@ -51,7 +51,7 @@ const Form = () => {
 
     return (
     <form onSubmit={handleFormSubmit} class="flex w-full justify-center items-end py-3" >
-        <div class="relative mr-4 lg:w-full xl:w-1/2 w-2/4 md:w-full text-left">
+        <div class="relative mr-4 2xl:w-6/12 xl:w-7/12 lg:w-8/12 md:w-7/12 sm:w-7/12 mobile:w-8/12 text-left">
           <label for="hero-field" class="leading-7 text-sm text-white">Join The Waitlist</label>
           <input type="email" value={emailInput} onChange={(e) => {setEmailInput(e.target.value)}} id="hero-field" placeholder="Email" name="hero-field" class="w-full bg-gray-800 rounded border bg-opacity-40 border-gray-700 focus:ring-2 focus:ring-indigo-900 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
         </div>
--- pages/components/hero2.js
+++ pages/components/hero2.js
@@ -33,11 +33,11 @@ const Hero2 = ( ) => {
   
 
     return ( 
-<section class="text-gray-400 bg-gradient-to-r from-indigo-500 to-indigo-900 body-font h-screen">
+<section class="text-gray-400 bg-gradient-to-r from-indigo-500 to-indigo-900 body-font h-full">
     <Header></Header>
-  <div class="container mx-auto flex flex-col px-5 py-0 justify-center items-center">
+  <div class="container mx-auto flex flex-col py-0 justify-center items-center">
     <Image class=" mb-10 object-cover object-center rounded shadow-2xl 2xl:w-8/12 xl:w-9/12 lg:w-11/12 md:w-1/1 sm:w-1/1  " alt="hero" src={pic}/>
-    <div class="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
+    <div class="w-full 2xl:w-10/12 lg:w-9/12 md:w-11/12  flex flex-col mb-16 items-center text-center">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font- text-white"> <span ref={TypedElement}></span>Together, Anywhere</h1>
       <p class="mb-2 font-medium leading-relaxed text-white w-5/6"> Seamlessly synchronize browsing sessions with your team, Enable real-time sharing of insights, research, and interactions. Elevate productivity, deepen connections, and unlock shared online experiences like never before.</p>
       <Form></Form>
