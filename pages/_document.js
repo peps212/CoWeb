3ad4b05961652c3ecda92c505cd5fa94317dc8e0
peps212 <giuseppe.rapisarda12@gmail.com>
2023-08-25 10:59:25 +0100

    added google tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
       <meta name="google-site-verification" content="TVIENNQKxolHSER3Iz8lDEb8D0HAwCFuDkMvLzor1Eo" />
+      </Head>
       <body>
         <Main />
         <NextScript />
