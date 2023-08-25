3e5eee1346d84aaeae0495f594d62778940ca3bd
peps212 <giuseppe.rapisarda12@gmail.com>
2023-08-25 10:15:22 +0100

    title change

--- pages/components/hero2.js
+++ pages/components/hero2.js
@@ -39,11 +39,11 @@ const Hero2 = ( ) => {
     <Header></Header>
   <div class="container mx-auto flex flex-col justify-center items-center text-center">
     
-  <h1 class="title-font mobile:text-xl sm:text-4xl text-3xl mb-4 font-bold text-white">The Virtual Collaboration Playground: <br></br><span className="mobile:text-sm sm:text-2xl lg:text-3xl text-3xl"> Revolutionize <span ref={TypedElement}></span></span> </h1>
+  <h1 class="title-font mobile:text-xl sm:text-4xl text-3xl mb-4 font-bold text-white">Real-Time Collaboration On Any Web App:<br></br><span className="mobile:text-sm sm:text-2xl lg:text-3xl text-3xl"> Revolutionize <span ref={TypedElement}></span></span> </h1>
     <Image class=" mb-10 object-cover object-center rounded shadow-2xl 2xl:w-8/12 xl:w-9/12 lg:w-11/12 md:w-1/1 sm:w-1/1  " alt="hero" src={pic}/>
     <div class="w-full 2xl:w-10/12 lg:w-9/12 md:w-11/12  flex flex-col mb-16 items-center text-center">
       
-      <p class="mb-2 font-bold leading-relaxed text-gray-300 w-5/6"> Seamlessly synchronize web browsing sessions with your team, real-time collaboration on all the Web Apps you <span className="text-red-400 text-lg">Love</span>. <br></br> <br></br> Enable <span className="text-sky-300 ">Shared Online Experiences</span> Like Never Before.</p>
+      <p class="mb-2 font-bold leading-relaxed text-gray-300 w-5/6"> Seamlessly synchronize web browsing sessions with your team, unlock real-time collaboration on all the Web Apps you <span className="text-red-400 text-lg">Love</span>. <br></br> <br></br> Enable <span className="text-sky-300 ">Shared Online Experiences</span> Like Never Before.</p>
       <Form></Form>
       <p class="text-sm mt-2 text-white mb-8 w-full"></p>
 
